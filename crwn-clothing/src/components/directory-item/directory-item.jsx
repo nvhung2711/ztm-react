e65552fa81441e516a6fa0cff82e1228b86edfd8
Hung Nguyen <nvhung2711@gmail.com>
2025-06-13 08:49:47 +0930

    styled components for directory item and form input

--- crwn-clothing/src/components/directory-item/directory-item.jsx
+++ crwn-clothing/src/components/directory-item/directory-item.jsx
@@ -1,11 +1,11 @@
 import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.style';
 
 const DirectoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageurl, title } = category;
 
     return (
         <DirectoryItemContainer>
-            <BackgroundImage imageUrl={imageUrl} />
+            <BackgroundImage imageurl={imageurl} />
             <Body>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
